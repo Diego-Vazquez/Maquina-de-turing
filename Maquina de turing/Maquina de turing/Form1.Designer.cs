ff85be0d9b65aec0b0641d2fc2d3c1b8c0063722
Diego-Vazquez <57245520+Diego-Vazquez@users.noreply.github.com>
2021-01-19 18:43:49 -0700

    Maquina de turing en C#

---
+++ Maquina de turing/Maquina de turing/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace Maquina_de_turing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.txt11 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt13 = new System.Windows.Forms.TextBox();
+            this.txt12 = new System.Windows.Forms.TextBox();
+            this.txt21 = new System.Windows.Forms.TextBox();
+            this.txt22 = new System.Windows.Forms.TextBox();
+            this.txt20 = new System.Windows.Forms.TextBox();
+            this.txt19 = new System.Windows.Forms.TextBox();
+            this.txt18 = new System.Windows.Forms.TextBox();
+            this.txt17 = new System.Windows.Forms.TextBox();
+            this.txt16 = new System.Windows.Forms.TextBox();
+            this.txt15 = new System.Windows.Forms.TextBox();
+            this.txt14 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(253, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Paso a paso";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cargar Cadena";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Maquina de Turing";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(138, 128);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(22, 20);
+            this.txt3.TabIndex = 3;
+            this.txt3.Text = "#";
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(166, 128);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(22, 20);
+            this.txt4.TabIndex = 4;
+            this.txt4.Text = "#";
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(194, 128);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(22, 20);
+            this.txt5.TabIndex = 5;
+            this.txt5.Text = "#";
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(222, 128);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(22, 20);
+            this.txt6.TabIndex = 6;
+            this.txt6.Text = "#";
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(250, 128);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(22, 20);
+            this.txt7.TabIndex = 7;
+            this.txt7.Text = "#";
+            this.txt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt8
+            // 
+            this.txt8.Location = new System.Drawing.Point(278, 128);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(22, 20);
+            this.txt8.TabIndex = 8;
+            this.txt8.Text = "#";
+            this.txt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt9
+            // 
+            this.txt9.Location = new System.Drawing.Point(306, 128);
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(22, 20);
+            this.txt9.TabIndex = 9;
+            this.txt9.Text = "#";
+            this.txt9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt11
+            // 
+            this.txt11.Location = new System.Drawing.Point(362, 128);
+            this.txt11.Name = "txt11";
+            this.txt11.Size = new System.Drawing.Size(22, 20);
+            this.txt11.TabIndex = 10;
+            this.txt11.Text = "#";
+            this.txt11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt10
+            // 
+            this.txt10.Location = new System.Drawing.Point(334, 128);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(22, 20);
+            this.txt10.TabIndex = 11;
+            this.txt10.Text = "#";
+            this.txt10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(82, 128);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(22, 20);
+            this.txt1.TabIndex = 12;
+            this.txt1.Text = "#";
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(110, 128);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(22, 20);
+            this.txt2.TabIndex = 13;
+            this.txt2.Text = "#";
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt13
+            // 
+            this.txt13.Location = new System.Drawing.Point(110, 154);
+            this.txt13.Name = "txt13";
+            this.txt13.Size = new System.Drawing.Size(22, 20);
+            this.txt13.TabIndex = 24;
+            this.txt13.Text = ".";
+            this.txt13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt12
+            // 
+            this.txt12.Location = new System.Drawing.Point(82, 154);
+            this.txt12.Name = "txt12";
+            this.txt12.Size = new System.Drawing.Size(22, 20);
+            this.txt12.TabIndex = 23;
+            this.txt12.Text = ".";
+            this.txt12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt21
+            // 
+            this.txt21.Location = new System.Drawing.Point(334, 154);
+            this.txt21.Name = "txt21";
+            this.txt21.Size = new System.Drawing.Size(22, 20);
+            this.txt21.TabIndex = 22;
+            this.txt21.Text = ".";
+            this.txt21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt22
+            // 
+            this.txt22.Location = new System.Drawing.Point(362, 154);
+            this.txt22.Name = "txt22";
+            this.txt22.Size = new System.Drawing.Size(22, 20);
+            this.txt22.TabIndex = 21;
+            this.txt22.Text = ".";
+            this.txt22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt20
+            // 
+            this.txt20.Location = new System.Drawing.Point(306, 154);
+            this.txt20.Name = "txt20";
+            this.txt20.Size = new System.Drawing.Size(22, 20);
+            this.txt20.TabIndex = 20;
+            this.txt20.Text = ".";
+            this.txt20.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt19
+            // 
+            this.txt19.Location = new System.Drawing.Point(278, 154);
+            this.txt19.Name = "txt19";
+            this.txt19.Size = new System.Drawing.Size(22, 20);
+            this.txt19.TabIndex = 19;
+            this.txt19.Text = ".";
+            this.txt19.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt18
+            // 
+            this.txt18.Location = new System.Drawing.Point(250, 154);
+            this.txt18.Name = "txt18";
+            this.txt18.Size = new System.Drawing.Size(22, 20);
+            this.txt18.TabIndex = 18;
+            this.txt18.Text = ".";
+            this.txt18.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt17
+            // 
+            this.txt17.Location = new System.Drawing.Point(222, 154);
+            this.txt17.Name = "txt17";
+            this.txt17.Size = new System.Drawing.Size(22, 20);
+            this.txt17.TabIndex = 17;
+            this.txt17.Text = ".";
+            this.txt17.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt16
+            // 
+            this.txt16.Location = new System.Drawing.Point(194, 154);
+            this.txt16.Name = "txt16";
+            this.txt16.Size = new System.Drawing.Size(22, 20);
+            this.txt16.TabIndex = 16;
+            this.txt16.Text = ".";
+            this.txt16.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt15
+            // 
+            this.txt15.Location = new System.Drawing.Point(166, 154);
+            this.txt15.Name = "txt15";
+            this.txt15.Size = new System.Drawing.Size(22, 20);
+            this.txt15.TabIndex = 15;
+            this.txt15.Text = ".";
+            this.txt15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt14
+            // 
+            this.txt14.Location = new System.Drawing.Point(138, 154);
+            this.txt14.Name = "txt14";
+            this.txt14.Size = new System.Drawing.Size(22, 20);
+            this.txt14.TabIndex = 14;
+            this.txt14.Text = ".";
+            this.txt14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(151, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Analizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Cadena:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Caracteres validos:  1  B  A  x  #";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 414);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt13);
+            this.Controls.Add(this.txt12);
+            this.Controls.Add(this.txt21);
+            this.Controls.Add(this.txt22);
+            this.Controls.Add(this.txt20);
+            this.Controls.Add(this.txt19);
+            this.Controls.Add(this.txt18);
+            this.Controls.Add(this.txt17);
+            this.Controls.Add(this.txt16);
+            this.Controls.Add(this.txt15);
+            this.Controls.Add(this.txt14);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt11);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.TextBox txt11;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt13;
+        private System.Windows.Forms.TextBox txt12;
+        private System.Windows.Forms.TextBox txt21;
+        private System.Windows.Forms.TextBox txt22;
+        private System.Windows.Forms.TextBox txt20;
+        private System.Windows.Forms.TextBox txt19;
+        private System.Windows.Forms.TextBox txt18;
+        private System.Windows.Forms.TextBox txt17;
+        private System.Windows.Forms.TextBox txt16;
+        private System.Windows.Forms.TextBox txt15;
+        private System.Windows.Forms.TextBox txt14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Maquina de turing/Maquina de turing/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maquina_de_turing
+{
+    public partial class Form1 : Form
+    {
+        private String cadena;
+        private String cadena2;
+        private String auxiliar;
+        private int estado=0;
+        private bool correr = false;
+        string[] arreglo;
+        private int apuntador=1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            actualizar_apuntador();
+            correr = false;
+            button3.Enabled = false;
+            switch(estado){
+                case 0: { q0(); break; }
+                case 1: { q1(); break; }
+                case 2: { q2(); break; }
+                case 3: { q3(); break; }
+                case 4: { q4(); break; }
+            }
+        }
+
+        public void Apuntar()
+        {
+            switch (apuntador)
+            {
+                case 0: { txt12.Text = "^"; break; }
+                case 1: { txt13.Text = "^"; break; }
+                case 2: { txt14.Text = "^"; break; }
+                case 3: { txt15.Text = "^"; break; }
+                case 4: { txt16.Text = "^"; break; }
+                case 5: { txt17.Text = "^"; break; }
+                case 6: { txt18.Text = "^"; break; }
+                case 7: { txt19.Text = "^"; break; }
+                case 8: { txt20.Text = "^"; break; }
+                case 9: { txt21.Text = "^"; break; }
+                case 10: { txt22.Text = "^"; break; }
+            }
+        }
+        public void actualizar(string dato)
+        {
+            switch (apuntador)
+            {
+                case 1: { txt2.Text = dato; break; }
+                case 2: { txt3.Text = dato; break; }
+                case 3: { txt4.Text = dato; break; }
+                case 4: { txt5.Text = dato; break; }
+                case 5: { txt6.Text = dato; break; }
+                case 6: { txt7.Text = dato; break; }
+                case 7: { txt8.Text = dato; break; }
+                case 8: { txt9.Text = dato; break; }
+                case 9: { txt10.Text = dato; break; }
+                case 10: { txt11.Text = dato; break; }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals(""))
+            {
+                button1.Enabled = false;
+                button3.Enabled = false;
+                cadena = "";
+                cadena2 = "";
+                estado = 0;
+                apuntador = 1;
+                correr = false;
+                auxiliar = "";
+                MessageBox.Show("Aún no se a ingresado una cadena de texto", "Alerta");
+            }
+            else
+            {
+                cadena = "";
+                cadena2 = "";
+                estado = 0;
+                apuntador = 1;
+                correr = false;
+                auxiliar = "";
+                button1.Enabled = true;
+                button3.Enabled = true;
+                actualizar_apuntador();
+                cadena = textBox1.Text;
+            cadena2 = "#"+cadena+"#";
+            arreglo=new string[cadena2.Length];
+            for(int i=0;i<cadena2.Length;i++){
+                arreglo[i] = Convert.ToString(cadena2.ElementAt(i));
+            }
+            iniciar_cinta();
+            }
+           
+        }
+
+
+        public void derecha()
+        {
+            apuntador++;
+        }
+
+        public void isquierda()
+        {
+            apuntador--;
+        }
+
+        public void iniciar_cinta()
+        {
+            int posicion = 0;
+            while (posicion < cadena2.Length)
+            {
+                switch (posicion)
+                {
+                    case 0: { txt1.Text = arreglo[0]; break; }
+                    case 1: { txt2.Text = arreglo[1]; break; }
+                    case 2: { txt3.Text = arreglo[2]; break; }
+                    case 3: { txt4.Text = arreglo[3]; break; }
+                    case 4: { txt5.Text = arreglo[4]; break; }
+                    case 5: { txt6.Text = arreglo[5]; break; }
+                    case 6: { txt7.Text = arreglo[6]; break; }
+                    case 7: { txt8.Text = arreglo[7]; break; }
+                    case 8: { txt9.Text = arreglo[8]; break; }
+                    case 9: { txt10.Text = arreglo[9]; break; }
+                    case 10: { txt11.Text = arreglo[10]; break; }
+                   
+                }
+                posicion++;
+            }
+            txt12.Text = "^";
+        } 
+
+        public void actualizar_apuntador(){
+            txt12.Text = ".";
+            txt13.Text = ".";
+            txt14.Text = ".";
+            txt15.Text = ".";
+            txt16.Text = ".";
+            txt17.Text = ".";
+            txt18.Text = ".";
+            txt19.Text = ".";
+            txt20.Text = ".";
+            txt21.Text = ".";
+            txt22.Text = ".";
+        }
+        
+        // ************* estados ************************************************* 
+        //*************************************
+
+        public void q0(){
+            actualizar_apuntador();
+            Apuntar();
+            auxiliar = arreglo[apuntador];
+            if (auxiliar.Equals("1")) { arreglo[apuntador] = "x"; actualizar("x"); derecha(); if (correr) { q1(); estado = 1;  } else { estado = 1; } }
+            else if (auxiliar.Equals("B")) { arreglo[apuntador] = "B"; actualizar("B"); derecha(); if (correr) { q3(); estado = 3; } else { estado = 3; } }
+            else{button1.Enabled = false; button3.Enabled = false; MessageBox.Show("Caracter invalido: "+auxiliar, "Alerta");}
+        }
+
+        public void q1()
+        {
+            actualizar_apuntador();
+            Apuntar();
+            auxiliar = arreglo[apuntador];
+            if (auxiliar.Equals("1")) { arreglo[apuntador] = "1"; actualizar("1"); derecha(); if (correr) { q1(); estado = 1; } else { estado = 1; } }
+            else if (auxiliar.Equals("B")) { arreglo[apuntador] = "B"; actualizar("B"); derecha(); if (correr) { q1(); estado = 1; } else { estado = 1; } }
+            else if (auxiliar.Equals("A")) { arreglo[apuntador] = "B"; actualizar("B"); isquierda(); if (correr) { q2(); estado = 2; } else { estado = 2; } }
+            else { button1.Enabled = false; button3.Enabled = false; MessageBox.Show("Caracter invalido: "+auxiliar, "Alerta"); }
+
+        }
+
+        public void q2()
+        {
+            actualizar_apuntador();
+            Apuntar();
+            auxiliar = arreglo[apuntador];
+            if (auxiliar.Equals("1")) { arreglo[apuntador] = "1"; actualizar("1"); isquierda(); if (correr) { q2(); estado = 2; } else { estado = 2; } }
+            else if (auxiliar.Equals("B")) { arreglo[apuntador] = "B"; actualizar("B"); isquierda(); if (correr) { q2(); estado = 2; } else { estado = 2; } }
+            else if (auxiliar.Equals("x")) { arreglo[apuntador] = "1"; actualizar("1"); derecha(); if (correr) { q0(); estado = 0; } else { estado = 0; } }
+            else { button1.Enabled = false; button3.Enabled = false; MessageBox.Show("Caracter invalido: "+auxiliar, "Alerta"); }
+
+        }
+
+        public void q3()
+        {
+            actualizar_apuntador();
+            Apuntar();
+            auxiliar = arreglo[apuntador];
+            if (auxiliar.Equals("A")) { arreglo[apuntador] = "A"; actualizar("A"); derecha(); if (correr) { q3(); estado = 3;} else { estado = 3; } }
+            else if (auxiliar.Equals("B")) { arreglo[apuntador] = "B"; actualizar("B"); derecha(); if (correr) { q3(); estado = 3;  } else { estado = 3; } }
+            else if (auxiliar.Equals("#")) { arreglo[apuntador] = "#"; actualizar("#"); derecha(); if (correr) { q4(); estado = 4;  } else { estado = 4; } }
+            else { button1.Enabled = false; button3.Enabled = false; MessageBox.Show("Caracter invalido: "+auxiliar, "Alerta"); }
+        }
+
+        public void q4()
+        {
+
+            actualizar_apuntador();
+            Apuntar();
+            correr = false;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            MessageBox.Show("   Cadena Valida", " ");
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            correr = true;
+            q0();
+        }
+
+
+
+
+      
+    }
+}
